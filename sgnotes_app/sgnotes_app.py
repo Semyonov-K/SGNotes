88c9b0b26565b43d80c360ead76ff664ae974026
unknown <fixer20112@mail.com>
2023-06-07 23:05:47 +0300

    learn html

--- sgnotes_app/sgnotes_app.py
+++ sgnotes_app/sgnotes_app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from sgnotes_app import app, db
 
 
 @app.route('/')
 def my_index_view():
-    return 'Это мой первый Flask-проект'
+    return render_template('registration.html')
 
 if __name__ == '__main__':
     app.run()
