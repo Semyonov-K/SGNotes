c591a29b075fe84200bf05814e42c52fd0ac3af6
Semyonov-K <fixer20112@mail.ru>
2023-08-16 23:53:37 +0300

    need fix api

--- sgnotes_app/api_views.py
+++ sgnotes_app/api_views.py
@@ -1,27 +1,47 @@
 from flask import jsonify, request
 from flask_login import login_user, login_required, logout_user, current_user
+from functools import wraps
 from http import HTTPStatus
+import json
 
 from . import app, db
 from .models import Note, User
 from .error_handlers import InvalidAPIUsage
 
 
+def check_authentication(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if current_user.is_authenticated:
+            return func(*args, **kwargs)
+        else:
+            response = {
+                'error': 'Вы не авторизованы'
+            }
+            return json.dumps(response), HTTPStatus.UNAUTHORIZED, {'Content-Type': 'application/json'}
+    return wrapper
+
+
 @app.route('/api/notes/<int:id>/', methods=['GET'])
 @login_required
-def get_note(id):
-    note = Note.query.get(id)
-    if note is None:
-        raise InvalidAPIUsage(
-            'Заметка не найдена',
-            HTTPStatus.NOT_FOUND
-        )
-    return jsonify({'note': note.to_dict()}), HTTPStatus.OK
+@check_authentication
+def api_get_note(id):
+    if current_user.is_authenticated:
+        note = Note.query.get(id)
+        if note is None:
+            raise InvalidAPIUsage(
+                'Заметка не найдена',
+                HTTPStatus.NOT_FOUND
+            )
+        return jsonify({'note': note.to_dict()}), HTTPStatus.OK
+    else:
+        return jsonify({'message': 'Не авторизован'}), HTTPStatus.UNAUTHORIZED
 
 
 @app.route('/api/notes/<int:id>/', methods=['PATCH'])
 @login_required
-def update_note(id):
+@check_authentication
+def api_update_note(id):
     data = request.get_json()
     note = Note.query.get(id)
     if note is None:
@@ -39,7 +59,8 @@ def update_note(id):
 
 @app.route('/api/notes/<int:id>/', methods=['DELETE'])
 @login_required
-def delete_note(id):
+@check_authentication
+def api_delete_note(id):
     note = Note.query.get(id)
     if note is None:
         raise InvalidAPIUsage(
@@ -53,15 +74,17 @@ def delete_note(id):
 
 @app.route('/api/notes/', methods=['GET'])
 @login_required
-def get_notes():
+@check_authentication
+def api_get_notes():
     notes = Note.query.all()  
     notes_list = [note.to_dict() for note in notes]
     return jsonify({'notes': notes_list}), HTTPStatus.OK
 
 
-@app.route('/api/notes/', methods=['POST'])
+@app.route('/api/add_note/', methods=['POST'])
 @login_required
-def add_note():
+@check_authentication
+def api_add_note():
     data = request.get_json()
     if 'title' not in data or 'text' not in data:
         raise InvalidAPIUsage('В запросе отсутствуют обязательные поля')
@@ -73,7 +96,7 @@ def add_note():
 
 
 @app.route('/api/register', methods=['POST'])
-def register():
+def api_register():
     data = request.get_json()
     users = User.query.filter(User.username==data['username']).first()
     if users:
@@ -89,7 +112,7 @@ def register():
 
 
 @app.route('/api/login', methods=['POST'])
-def login():
+def api_login():
     data = request.get_json()
     user = User.query.filter_by(username=data['username']).first()
     if user and user.password == data['password']:
@@ -103,14 +126,16 @@ def login():
 
 @app.route('/api/logout', methods=['POST'])
 @login_required
-def logout():
+@check_authentication
+def api_logout():
     logout_user()
     return jsonify({'message': 'Вы вышли из профиля'})
 
 
 @app.route('/api/change_password', methods=['GET', 'POST'])
 @login_required
-def change_password():
+@check_authentication
+def api_change_password():
     user_id = current_user.id
     user = User.query.get(user_id)
     if not user:
